00575fc99153dea5661beb81bfe3432d94e898d2
Mike Rehner <babarehner@gmail.com>
2018-03-16 18:27:54 -0400

    add icons for plus sign and check mark. Add menu items to show check mark

--- app/src/main/java/com/babarehner/android/xminder/StrengthExActivity.java
+++ app/src/main/java/com/babarehner/android/xminder/StrengthExActivity.java
@@ -2,18 +2,25 @@ package com.babarehner.android.xminder;
 
 import android.app.DatePickerDialog;
 import android.app.LoaderManager;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 
@@ -133,7 +140,6 @@ public class StrengthExActivity extends AppCompatActivity implements LoaderManag
     }
 
 
-
     //updates the date displayed in TextView
     private void updateDate() {
         tvDate.setText(
@@ -154,5 +160,112 @@ public class StrengthExActivity extends AppCompatActivity implements LoaderManag
                     updateDate();
                 }
             };
+
+    //TODO change save checkmart to white color
+    @Override   // show the options menu
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.menu_edit, m);
+        return true;
+    }
+
+    @Override   // hide delete menu item when adding a new exercise
+    public boolean onPrepareOptionsMenu(Menu m) {
+        super.onPrepareOptionsMenu(m);
+        // if this is add an exercise, hide "delete" menu item
+        if (mCurrentStrengthExUri == null) {
+            MenuItem menuItem = m.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
+    }
+
+
+    /*
+    @Override        // Select from the options menu
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                saveExercise();
+                finish();       // exit activity
+                return true;
+            case R.id.action_delete:
+                // Alert Dialog for deleting one book
+                showDeleteConfirmationDialog();
+                return true;
+            // this is the <- button on the header
+            case android.R.id.home:
+                // book has not changed
+                if (!mExerciseChanged) {
+                    NavUtils.navigateUpFromSameTask(StrengthExActivity.this);
+                    return true;
+                }
+                // set up dialog to warn user about unsaved changes
+                DialogInterface.OnClickListener discardButtonClickListener =
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                //user click discard. Navigate up to parent activity
+                                NavUtils.navigateUpFromSameTask(StrengthExActivity.this);
+                            }
+                        };
+                // show user they have unsaved changes
+                showUnsavedChangesDialog(discardButtonClickListener);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    */
+
+
+    /*
+    private void saveExercise(){
+
+        // read from EditText input fields
+        String titleString = mTitleEditText.getText().toString().trim();
+        String authorString = mAuthorEditText.getText().toString().trim();
+        //String publishYearString = String.valueOf(mPublishYear);
+        String borrowerString = mBorrowerEditText.getText().toString().trim();
+
+        // if adding book and the title field is left blank do nothing
+        if (mCurrentBookUri == null && TextUtils.isEmpty(titleString)) {
+            Toast.makeText(this, getString(R.string.missing_book_title),
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        ContentValues values = new ContentValues();
+        values.put(LibraryContract.LibraryEntry.COL_TITLE, titleString);
+        values.put(LibraryContract.LibraryEntry.COL_AUTHOR, authorString);
+        values.put(LibraryContract.LibraryEntry.COL_YEAR_PUBLISHED, mPublishYear);
+        //Log.v("BookActivity"," mPublishYear " + mPublishYear);
+        values.put(LibraryContract.LibraryEntry.COL_STATUS, borrowerString);
+
+        if (mCurrentBookUri == null) {
+            // a new book
+            Uri newUri = getContentResolver().insert(LibraryContract.LibraryEntry.CONTENT_URI, values);
+            if (newUri == null) {
+                Toast.makeText(this, getString(R.string.library_provider_insert_book_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getString(R.string.library_provider_insert_book_good),
+                        Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // existing book so update with content URI and pass in ContentValues
+            int rowsAffected = getContentResolver().update(mCurrentBookUri, values, null, null);
+            if (rowsAffected == 0) {
+                // TODO Check db- Text Not Null does not seem to be working or entering
+                // "" does not mean NOT Null- there must be an error message closer to the db!!!
+                Toast.makeText(this, getString(R.string.edit_update_book_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, getString(R.string.edit_update_book_success),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+    */
+
 
 }
